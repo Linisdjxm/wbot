b07995171049b1c4ce6c4bf5ccafdca06c6896b6
Linisdjxm <35957435+Linisdjxm@users.noreply.github.com>
2023-07-31 13:25:44 +0800

    Update refdate.py

--- refdate.py
+++ refdate.py
@@ -13,14 +13,18 @@ limit = 10
 month = "(?:January|February|March|April|May|June|July|August|September|October|November|December)"
 re1 = r'''\|[^|]*?date[\s]*=[\s]*''' + month + r''' [0-9]+,[\s]*[0-9][0-9][0-9][0-9]'''
 re2 = r'''\|[^|]*?date[\s]*=[\s]*''' + month + r''' [0-9][0-9][0-9][0-9]'''
+re3 = r'''\|[^|]*?date[\s]*=[\s]*''' + r'''[0-9]+ ''' + month + r'''[\s]*[0-9][0-9][0-9][0-9]'''
 re1a = month + r''' [0-9]+,[\s]*[0-9][0-9][0-9][0-9]'''
 re2a = month + r''' [0-9][0-9][0-9][0-9]'''
+re3a = r'''[0-9]+ ''' + month + r'''[\s]*[0-9][0-9][0-9][0-9]'''
 
 re1c = re.compile(re1)
 re2c = re.compile(re2)
+re3c = re.compile(re3)
+
 re1ac = re.compile(re1a)
 re2ac = re.compile(re2a)
-
+re3ac = re.compile(re3a)
 
 PARAMS = {
     "action": "query",
@@ -42,7 +46,8 @@ while "continue" in DATA.keys():
         #print(re1)
         res = re.findall(re1c,text)
         res2 = re.findall(re2c,text)
-        print(title + " : " + str(len(res) + len(res2)))
+        res3 = re.findall(re3c,text)
+        print(title + " : " + str(len(res) + len(res2) + len(res3)))
         for item2 in res:
             src = re.findall(re1ac,item2)[0]
             text = re.sub(src,dateparser.parse(src).isoformat()[:-9],text)
@@ -51,6 +56,10 @@ while "continue" in DATA.keys():
             src = re.findall(re2ac,item3)[0]
             text = re.sub(src,dateparser.parse(src).isoformat()[:-12],text)
             print(src + " --> " + dateparser.parse(src).isoformat()[:-12])
+        for item4 in res3:
+            src = re.findall(re3ac,item4)[0]
+            text = re.sub(src,dateparser.parse(src).isoformat()[:-9],text)
+            print(src + " --> " + dateparser.parse(src).isoformat()[:-9])
         #print(text)
         wlib.wrwt(title,S,text,"Date Correct Test",CSRF_TOKEN)
         break
