cb6c15bd12c8e3dac94f53a4ba7b3d95e603cbdc
Linisdjxm <35957435+Linisdjxm@users.noreply.github.com>
2024-07-23 23:08:37 +0800

    Update moe2.js

--- moe2.js
+++ moe2.js
@@ -7,7 +7,19 @@
                 'FunctionH');
 
         var api = new mw.Api();
-
+        if(document.querySelector('.' + 'permissions-errors') != null)
+        {
+            var htextx = document.querySelector('.' + 'permissions-errors').innerText.match(/#[0-9]*/)[0].substring(1);
+            var params = {
+                action: 'parse',
+                oldid: +htextx,
+                format: 'json'
+            };
+            api.get(params).done(data => {
+                document.getElementById('bodyContent').innerHTML = data.parse.text['*'];
+                console.log("Success");
+            });
+        }
         // Bind click handler
         $(portletLinkOnline).find('a').click(function(e) {
             e.preventDefault();
